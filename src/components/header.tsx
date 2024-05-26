4ca1c2f3f52e375d4523cd6b08f5e5e2b81722c0
Shoaib Ahmed <gameranonymous338@gmail.com>
2024-05-26 18:17:32 +0500

    Implement Auth

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,43 @@
+import { signIn, signOut } from "@/lib/auth";
 import Link from "next/link";
+import { auth } from "@/lib/auth";
 
-function Header() {
+async function Header() {
+  const session = await auth();
   return (
     <header className="bg-blue-500 sticky top-0 left-0 w-full h-20 flex items-start justify-center">
       <nav className="w-[80%] h-full flex items-center justify-between">
         <Link href={"/"} className="text-3xl font-bold text-white">
           Blogify
         </Link>
-        <ul className="flex items-center justify-center gap-10 text-lg">
-          <li>
-            <Link href={"/blogs"} className="text-white hover:text-gray-300">
-              Blogs
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/api/auth/login"}
-              className="text-white hover:text-gray-300"
+        <div className="flex items-center justify-center gap-10 text-lg">
+          <Link href={"/blogs"} className="text-white hover:text-gray-300">
+            Blogs
+          </Link>
+          {!session?.user ? (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
             >
-              Login
-            </Link>
-          </li>
-        </ul>
+              <button className="text-white hover:text-gray-300" type="submit">
+                Login
+              </button>
+            </form>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button className="text-white hover:text-gray-300" type="submit">
+                Log out
+              </button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
